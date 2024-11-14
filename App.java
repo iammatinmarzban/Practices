b169aed24b571596bd7c7d17609c1816a0f2c501
iammatinmarzban <matinmarzban1271@gmail.com>
2024-11-15 02:57:41 +0330

    cleanded counter

--- App.java
+++ App.java
@@ -7,7 +7,6 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         System.out.println("enter a sentense:");
         String input = scanner.nextLine();
-        int count = 0;
         String newStr = "";
         int lastspaceInSentence = input.lastIndexOf(' ');
         int lastCharachter = input.length();
@@ -15,8 +14,6 @@ public class App {
             newStr = newStr.concat(" ").concat(input.substring(lastspaceInSentence + 1, lastCharachter));
             lastCharachter = lastspaceInSentence;
             lastspaceInSentence = input.lastIndexOf(' ', lastspaceInSentence - 1);
-            count++;
-            System.out.println(count);
         }
         System.out.println(newStr + " " + input.substring(0, lastCharachter));
 
